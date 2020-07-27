0acb94535b32bd24bf2a9cfa0e5967ebd88e777d
Silmos <quan.pham,d@gmail.com>
2020-07-27 17:10:14 +0200

    finishing code, commented debug print out, fixed interface

--- faqchat.lua
+++ faqchat.lua
@@ -1,3 +1,4 @@
+-- Badly programmed by Silmos (I know its bad, dont judge me pls)
 -- Global
 faqchatConfig = {}
 
@@ -7,10 +8,11 @@ SLASH_FAQCHAT3 = "/faq"
 
 local frame = CreateFrame("Frame");
 frame:RegisterEvent("ADDON_LOADED");
+version = "v 1.0.0"
 
 function frame:OnEvent(event, arg1)
     if event == "ADDON_LOADED" and arg1 == "faqchat" then
-        print("FAQChat Loaded");
+        print("FAQChat "..version.. " by Silmos [LOADED]");
         RenderOptions();
     end
 end
@@ -22,8 +24,8 @@ frame2:RegisterEvent("CHAT_MSG_GUILD")
 frame2:SetScript("OnEvent", function(e, event, mess, sender)
     for key, value in pairs(faqchatConfig.buzz) do
         if key == nil or key == "" then
-            print("Ignore Nil")
-        else --mess:lower():match(key)
+            --print("Ignore Nil")
+        else
             if event == "CHAT_MSG_WHISPER" and string.match(mess:lower(), key:lower()) and faqchatConfig.checkwhisper[key] == true then
                 SendChatMessage(value ,"WHISPER" ,"language" ,sender);
             end
@@ -32,8 +34,8 @@ frame2:SetScript("OnEvent", function(e, event, mess, sender)
             end
         end
     end
-    print(mess);
-    print(sender);
+    --print(mess);
+    --print(sender);
 end)
 
 local function OpenConfig()
@@ -49,28 +51,29 @@ function RenderOptions()
     -- Header
     local MessageHeader = ConfigFrame:CreateFontString(nil, "ARTWORK","GameFontNormalLarge");
     MessageHeader:SetPoint("TOPLEFT", 10, -10);
-    MessageHeader:SetText("FAQChat");
+    MessageHeader:SetText("FAQChat "..version);
     -- BuzzProfile
     local InputBuzzHeader = ConfigFrame:CreateFontString(nil, "ARTWORK","GameFontNormal");
-    InputBuzzHeader:SetPoint("TOPLEFT", 10, -40);
-    InputBuzzHeader:SetText("New BuzzProfile");
+    InputBuzzHeader:SetPoint("TOPLEFT", 40, -45);
+    InputBuzzHeader:SetText("New Buzzword");
     InputBuzzProfile = CreateFrame("EditBox", "InputBuzz", ConfigFrame, "InputBoxTemplate");
-    InputBuzzProfile:SetSize(100,100);
+    InputBuzzProfile:SetSize(150,100);
     InputBuzzProfile:SetAutoFocus(false);
-    InputBuzzProfile:SetPoint("TOPLEFT",ConfigFrame, "TOPLEFT", 10, -15);
+    InputBuzzProfile:SetPoint("TOPLEFT",ConfigFrame, "TOPLEFT", 15, -25);
     -- BuzzMessage
     local InputTextHeader = ConfigFrame:CreateFontString(nil, "ARTWORK","GameFontNormal");
-    InputTextHeader:SetPoint("TOPLEFT", 10, -75);
+    InputTextHeader:SetPoint("TOPLEFT", 40, -100);
     InputTextHeader:SetText("Input Text");
     InputTextBox = CreateFrame("EditBox", "InputText", ConfigFrame, menu);
     InputTextBox:SetSize(400,400);
     InputTextBox:SetAutoFocus(false);
     InputTextBox:SetMultiLine(true);
-    InputTextBox:SetPoint("TOPLEFT",ConfigFrame, "TOPLEFT", 10, -90);
+    InputTextBox:SetPoint("TOPLEFT",ConfigFrame, "TOPLEFT", 10, -120);
+    InputTextBox:SetPoint("BOTTOMLEFT",ConfigFrame, "BOTTOMLEFT", 10, 320);
     InputTextBox:SetBackdrop({
 	    bgFile = [[Interface\Buttons\WHITE8x8]],
 	    edgeFile = [[Interface\Tooltips\UI-Tooltip-Border]],
-	    edgeSize = 14,
+	    edgeSize = 12,
 	    insets = {left = 3, right = 3, top = 3, bottom = 3},
     })
     InputTextBox:SetBackdropColor(0, 0, 0)
@@ -78,31 +81,31 @@ function RenderOptions()
     InputTextBox:SetFont("Fonts\\FRIZQT__.TTF", 12)
     -- Whisper Chechbox
     WhisperCheckbox = CreateFrame("CheckButton", "WhisperCheckBox", ConfigFrame, "ChatConfigCheckButtonTemplate");
-	WhisperCheckbox:SetPoint("TOPLEFT", 10, -125)
+	WhisperCheckbox:SetPoint("TOPLEFT", 425, -125)
     getglobal(WhisperCheckbox:GetName() .. 'Text'):SetText("Whisper");
     -- Guildchat Checkbox
     GuildchatCheckbox = CreateFrame("CheckButton", "GuildchatCheckBox", ConfigFrame, "ChatConfigCheckButtonTemplate");
-	GuildchatCheckbox:SetPoint("TOPLEFT", 10, -145)
+	GuildchatCheckbox:SetPoint("TOPLEFT", 425, -145)
     GuildchatCheckbox:SetText("Guildchat");
     getglobal(GuildchatCheckbox:GetName() .. 'Text'):SetText("Guild Chat");
     -- Save Button
     local SaveButton = CreateFrame("Button", "SaveButton", ConfigFrame, "OptionsButtonTemplate");
-    SaveButton:SetPoint("TOPLEFT", 100, -10);
+    SaveButton:SetPoint("TOPLEFT", 200, -30);
     SaveButton:SetText("Save");
     SaveButton:SetScript("OnClick", CreateBuzzProfile);
     -- Load Button
     local LoadButton = CreateFrame("Button", "LoadButton", ConfigFrame, "OptionsButtonTemplate");
-    LoadButton:SetPoint("TOPLEFT", 100, -40);
+    LoadButton:SetPoint("TOPLEFT", 200, -55);
     LoadButton:SetText("Load");
     LoadButton:SetScript("OnClick", LoadBuzzProfile);
     -- Delete Button
     local DeleteButton = CreateFrame("Button", "LoadButton", ConfigFrame, "OptionsButtonTemplate");
-    DeleteButton:SetPoint("TOPLEFT", 100, -60);
+    DeleteButton:SetPoint("TOPLEFT", 200, -80);
     DeleteButton:SetText("Delete");
     DeleteButton:SetScript("OnClick", DeleteProfile);
     -- DropDownBuzzProfile
     BuzzProfileDropDown = L_Create_UIDropDownMenu("BuzzProfileDropDown", ConfigFrame);
-	BuzzProfileDropDown:SetPoint("TOPLEFT", 500, -110)
+	BuzzProfileDropDown:SetPoint("TOPLEFT", 300, -55)
 	L_UIDropDownMenu_Initialize(BuzzProfileDropDown, BuzzProfileDropDownInitialize);
 	L_UIDropDownMenu_SetWidth(BuzzProfileDropDown, 150);
 	L_UIDropDownMenu_SetButtonWidth(BuzzProfileDropDown, 124);
@@ -115,19 +118,19 @@ faqchatConfig.checkwhisper = {}
 faqchatConfig.checkguild = {}
 
 function CreateBuzzProfile()
-    print("Created new profile");
+    --print("Created new profile");
     buzzid = InputBuzzProfile:GetText();
-    print(buzzid);
+    --print(buzzid);
     if buzzid == nil or buzzid == "" then
-        message("BuzzID ist leer")
+        message("Buzzword ist leer")
     else
         faqchatConfig.buzz[buzzid] = InputTextBox:GetText();
-        print(faqchatConfig.buzz[buzzid]);
+        --print(faqchatConfig.buzz[buzzid]);
         faqchatConfig.checkwhisper[buzzid] = WhisperCheckbox:GetChecked();
-        print(faqchatConfig.checkwhisper[buzzid]);
+        --print(faqchatConfig.checkwhisper[buzzid]);
         faqchatConfig.checkguild[buzzid] = GuildchatCheckbox:GetChecked();
-        print(faqchatConfig.checkguild[buzzid]);
-        print("Done");
+        --print(faqchatConfig.checkguild[buzzid]);
+        --print("Done");
         BuzzProfileDropDownInitialize();
         InputBuzzProfile:SetText("");
         InputTextBox:SetText("");
@@ -137,7 +140,7 @@ end
 function BuzzProfileDropDownInitialize(self, level)
     for key, value in pairs(faqchatConfig.buzz) do
         if key == nil or key == "" or value == nil or value == "" then
-            print("Ignore Nil")
+            --print("Ignore Nil")
         else
             local InterfaceOptions_AddCategory
             info = L_UIDropDownMenu_CreateInfo();
@@ -147,15 +150,15 @@ function BuzzProfileDropDownInitialize(self, level)
                 L_UIDropDownMenu_SetSelectedValue(BuzzProfileDropDown, self.value);
             end
             L_UIDropDownMenu_AddButton(info, level);
-            print(key);
-            print(value);
+            --print(key);
+            --print(value);
         end
     end
 end
 
 function LoadBuzzProfile()
     local LoadID = L_UIDropDownMenu_GetText(BuzzProfileDropDown)
-    print(LoadID)
+    --print(LoadID)
     InputBuzzProfile:SetText(LoadID);
     InputTextBox:SetText(faqchatConfig.buzz[LoadID]);
     WhisperCheckbox:SetChecked(faqchatConfig.checkwhisper[LoadID]);
@@ -164,10 +167,14 @@ end
 
 function DeleteProfile()
     local LoadID = L_UIDropDownMenu_GetText(BuzzProfileDropDown)
-    print(LoadID)
+    --print(LoadID)
     faqchatConfig.buzz[LoadID] = nil
     faqchatConfig.checkwhisper[LoadID] = nil
     faqchatConfig.checkguild[LoadID] = nil
+    InputBuzzProfile:SetText("");
+    InputTextBox:SetText("");
+    WhisperCheckbox:SetChecked(false);
+    GuildchatCheckbox:SetChecked(false);
 end
 
 SlashCmdList["FAQCHAT"] = OpenConfig;
